e9dd73748ec046b531707a3b109c3b3764c019e6
MateuszMaciaszczyk <maciaszczyk@student.agh.edu.pl>
2024-07-22 18:27:24 +0200

    Initial commit

---
+++ src/main/java/org/example/githubrepositorylist/GithubRepositoryListApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubrepositorylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositoryListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/githubrepositorylist/GithubRepositoryListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubrepositorylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
