f7ca73bc951a594da73c3686e6b0243158d90bd4
MateuszMaciaszczyk <maciaszczyk@student.agh.edu.pl>
2024-07-25 19:12:56 +0200

    added json

--- src/main/java/org/example/githubrepositorylist/model/BranchDetails.java
+++ src/main/java/org/example/githubrepositorylist/model/BranchDetails.java
@@ -1,29 +1,3 @@
 package org.example.githubrepositorylist.model;
 
-public class BranchDetails {
-    private String name;
-    private String commitSha;
-
-    // Constructors, getters and setters
-
-    public BranchDetails(String name, String commitSha) {
-        this.name = name;
-        this.commitSha = commitSha;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCommitSha() {
-        return commitSha;
-    }
-
-    public void setCommitSha(String commitSha) {
-        this.commitSha = commitSha;
-    }
-}
+public record BranchDetails(String name, String commitSha) {}
--- src/main/java/org/example/githubrepositorylist/model/RepositoryDetails.java
+++ src/main/java/org/example/githubrepositorylist/model/RepositoryDetails.java
@@ -2,40 +2,4 @@ package org.example.githubrepositorylist.model;
 
 import java.util.List;
 
-public class RepositoryDetails {
-    private String name;
-    private String ownerLogin;
-    private List<BranchDetails> branches;
-
-    // Constructors, getters and setters
-
-    public RepositoryDetails(String name, String ownerLogin, List<BranchDetails> branches) {
-        this.name = name;
-        this.ownerLogin = ownerLogin;
-        this.branches = branches;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getOwnerLogin() {
-        return ownerLogin;
-    }
-
-    public void setOwnerLogin(String ownerLogin) {
-        this.ownerLogin = ownerLogin;
-    }
-
-    public List<BranchDetails> getBranches() {
-        return branches;
-    }
-
-    public void setBranches(List<BranchDetails> branches) {
-        this.branches = branches;
-    }
-}
+public record RepositoryDetails (String name, String ownerLogin, List<BranchDetails> branches) {}
